d202339e2024ef8b3e9b29c54902f01785b9aea9
Onurene <onurene22@gmail.com>
2023-11-25 11:34:38 -0800

    Removed commented code

--- enroll/api.py
+++ enroll/api.py
@@ -474,24 +474,6 @@ def drop_student_administratively(instructorid: int, classid: int, sectionid: in
 
 ################# endpoint-9 #################
 
-#############SQLITE################
-# @app.get("/waitlist/{instructorid}/{classid}/{sectionid}/{name}/{username}/{email}/{roles}")
-# def view_waitlist(instructorid: int, classid: int, sectionid: int, name: str, username: str, email: str, roles: str, db: sqlite3.Connection = Depends(get_db)):
-#     roles = [word.strip() for word in roles.split(",")]
-#     check_user(instructorid, username, name, email, roles, db)
-#     instructor_class = db.execute("SELECT * FROM InstructorClasses WHERE classID=? AND SectionNumber=?",(classid,sectionid)).fetchone()
-#     if not instructor_class:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND, detail="Instructor does not have this class"
-#         )
-    
-#     query = "SELECT * FROM Students INNER JOIN (SELECT StudentID, Position FROM Waitlists WHERE ClassID = ? AND SectionNumber =? ORDER BY Position) as w on Students.StudentID = w.StudentID"
-#     waitlist = db.execute(query, (classid, sectionid)).fetchall()
-#     if not waitlist:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No students found in the waitlist for this class")
-#     return {"Waitlist": [{"student_id": student["StudentID"]} for student in waitlist]}
-
-#############DYNAMODB################
 @app.get("/waitlist/instructor/{instructorid}/{classid}/{sectionid}/{name}/{username}/{email}/{roles}")
 def view_waitlist(instructorid: int, classid: int, sectionid: int, name: str, username: str, email: str, roles: str):
 
